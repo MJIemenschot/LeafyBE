7172b46fb4ee3dca0eb242282fca9174403b8203
MJIemenschot <m.iemenschot@outlook.com>
2021-09-01 17:13:28 +0200

    // cleaned

---
+++ src/main/java/com/example/xedd/config/CorsConfiguration.java
@@ -0,0 +1,17 @@
+//package com.example.xedd.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class CorsConfiguration implements WebMvcConfigurer
+//{
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/public/**")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE");
+//    }
+//}
