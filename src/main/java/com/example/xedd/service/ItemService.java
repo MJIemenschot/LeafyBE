50f6f64ed1ac9bcd4cea6c93cad637ca2a8b16aa
MJIemenschot <m.iemenschot@outlook.com>
2021-09-02 15:47:33 +0200

    items

--- src/main/java/com/example/xedd/service/ItemService.java
+++ src/main/java/com/example/xedd/service/ItemService.java
@@ -17,27 +17,15 @@ import java.util.Optional;
 public interface ItemService {
     void init();
 
-    //Zaaim
-   // public saveItem(MultipartFile file, String name, String description, String toPicture);
-
-    //Item addItemToUser(Long id, String username);
     List<Item> getAllItems();
     Collection<Item> getItems(String name);
     Item getItem(Long id);
     ItemResponseDto getItemById(long id);
     void deleteItem(long id);
-
-    //long createItem(Item item);
-    //Item saveItem(Item item);
-    //Item addItem(Item item, MultipartFile toPicture);
     long addItem(ItemRequestDto itemRequestDto);
-
     void updateItem(long id, Item item);
     Resource downloadFile(Long id);
     void partialUpdateItem(long id, Map<String, String> fields);
-    //public uploadFile(ItemResponseDto itemDto);
-    //uit FilestorageService
-   // uploadFile(MultipartFile toPicture);
     void deleteFile(String filename) throws IOException;
 
 
--- src/main/java/com/example/xedd/service/ItemServiceImpl.java
+++ src/main/java/com/example/xedd/service/ItemServiceImpl.java
@@ -46,10 +46,6 @@ public class ItemServiceImpl implements ItemService {
         }
     }
 
-
-    //    Path uploads = Paths.get(".\\uploads");
-//    private Long id;
-
     @Autowired
     private ItemRepository repository;
 
@@ -58,12 +54,6 @@ public class ItemServiceImpl implements ItemService {
         return repository.findAll();
     }
 
-
-
-//    @Override
-//    public Item saveItem(Item item) {
-//       return repository.save(item);
-//    };
 
 
     public long addItem(ItemRequestDto itemRequestDto) {
@@ -92,72 +82,13 @@ public class ItemServiceImpl implements ItemService {
     }
 
 
-//    @Override
-//    public Item addItemToUser(Long id, String username) {
-//        if (!repository.existsById(id))throw new NotFoundException();
-//
-//        Item item = repository.getById(id);
-//        Item newItem = new Item();
-//
-//        newItem.setUser(username);
-//        newItem.setSeed(false);
-//        newItem.setEnt(false);
-//        newItem.setPlant(false);
-//        newItem.setName(item.getName());
-//        newItem.setDescription(item.getDescription());
-//        newItem.setToPicture(item.getToPicture());
-//
-//        return repository.save(newItem);
-//
-//    }
-
-//    @Override
-//    public long createItem(Item item) {
-//        Item newItem = repository.save(item);
-//        return newItem.getId();
-//    }
-
-    //Zaaim
-    //public saveItem( String name, String description, String toPicture){
-     //   Item item = new Item();
-        //String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//        if(fileName.contains("..")){
-//            System.out.println("not a valid file");
-//        }
-//        try {
-//            item.setToPicture(Base64.getEncoder().encodeToString(file.getBytes()));
-//        }catch (IOException e){
-//            e.printStackTrace();
-//        }
-       // return repository.save(item);
-
-
-
-//    }
-//    @Override
-//    public void uploadFile(MultipartFile toPicture) {
-//        try {
-//            Path copyLocation = Paths.get(uploads + File.separator + StringUtils.cleanPath(toPicture.getOriginalFilename()));
-//            Files.copy(toPicture.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
-////            Item item = new Item();
-////            item.setDescription(toPicture.getName());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            throw new FileStorageException("Could not store file " + toPicture.getOriginalFilename() + ". Please try again.");
-//        }
-//    }
-
     @Override
     public void deleteFile(String filename) throws IOException {
         Path deleteLocation = Paths.get(uploads + File.separator + StringUtils.cleanPath(filename));
         Files.delete(deleteLocation);
     }
 
-//    @Override
-//    public void deleteFile(String filename) throws IOException {
-//        Path deleteLocation = Paths.get(uploads + File.separator + StringUtils.cleanPath(filename));
-//        Files.delete(deleteLocation);
-//    }
+
 
     public Collection<Item> getItems(String name) {
         if (name.isEmpty()) {
@@ -251,44 +182,12 @@ public class ItemServiceImpl implements ItemService {
         }
         return null;
     }
-//    @Override
-//    public Collection<Item> getItems(String name, String description) {
-//        return itemRepository.findAll();
-//    }
-//    public Collection<Item> getItems(String name, String description) {
-//        if (!name.isEmpty()) {
-//            if (!name.isEmpty()) {
-//                return itemRepository.findAllByNameAndDescription(name, description);
-//            }
-//            else {
-//                return itemRepository.findAllByName(name);
-//            }
-//        }
-//        else {
-//            if (!description.isEmpty()) {
-//                return itemRepository.findAllByDescription(description);
-//            }
-//            else {
-//                return itemRepository.findAll();
-//            }
-//        }
 
 
     @Override
     public boolean itemExistsById(long id) {
         return  repository.existsById(id);
     }
-
-//    @Override
-//    public List<Object> getAllSeeds() {
-//        List<Item> itemList = repository.findAll();
-//        List<Object> seeds = new ArrayList<>();
-//        for (int i = 0; i < itemList.size(); i++) {
-//            if (itemList.get(i).isSeed()) seeds.add(itemList.get(i));
-//        }
-//        return seeds;
-//    }
-
 
 }
 
